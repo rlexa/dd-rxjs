2810dd50bcb504cdc7ec7f2a61bdd4166b8c5feb
Alex Rempel <alexey.rempel@gmail.com>
2018-10-25 15:00:32 +0200

    add npmignore, jest testing, test

---
+++ lib/done-subject.spec.ts
@@ -0,0 +1,27 @@
+import { DoneSubject } from './done-subject';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
+describe('DoneSubject', () => {
+  test('done() completes', () => {
+    const done$ = new DoneSubject();
+    expect(done$.isStopped).toBe(false);
+    done$.done();
+    expect(done$.isStopped).toBe(true);
+  });
+
+  test('done() fires', () => {
+    const done$ = new DoneSubject();
+    const waitFor$ = new Subject();
+    const waitSubscription = waitFor$.pipe(takeUntil(done$)).subscribe();
+    expect(done$.isStopped).toBe(false);
+    expect(waitFor$.isStopped).toBe(false);
+    expect(waitSubscription.closed).toBe(false);
+    done$.done();
+    expect(done$.isStopped).toBe(true);
+    expect(waitFor$.isStopped).toBe(false);
+    expect(waitSubscription.closed).toBe(true);
+    waitFor$.complete();
+    expect(waitFor$.isStopped).toBe(true);
+  });
+});
